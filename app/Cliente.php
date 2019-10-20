ecf93f2aff19ac0922f2ae049ce3172df847d6f3
Leonardo Costa Sabino <leonardosabinoti@gmail.com>
2019-10-20 12:42:53 -0300

    Segundo Commit

--- app/Cliente.php
+++ app/Cliente.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    protected $fillable = ['aporte', 'nome', 'idade', 'sexo', 'idade_aposentado', 'renda_atual', 'final_desejado', 'contribuicao_mensal'];
+    protected $fillable = ['aporte', 'nome', 'idade', 'sexo', 'idade_aposentado', 'renda_atual', 'final_desejado', 'contribuicao_mensal', 'perfil', 'valor_acumulado', 'reservaEst'];
 }
